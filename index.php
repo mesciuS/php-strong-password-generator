59f6f7d15163cc29431fd89f7d96e9dce6356e61
mesciuS <giovadev@gmail.com>
2023-04-19 17:01:19 +0200

    feat: milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+function random_password( $length = 8 ) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+    $password = substr( str_shuffle( $chars ), 0, $length );
+    return $password;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +13,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    input {
+        width: 300px;
+    }
+</style>
 <body>
-    
+<!-- Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+Milestone 3 (BONUS)
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente. -->
+<form action="" method="GET">
+    <input type="number" name="password" placeholder="Di quanti caratteri la vuoi la password?">
+    <button type="submit">Genera</button>
+</form>
+<div>
+    <p>Ecco la tua password generata randomicamente:</p>
+    <?php echo $password = random_password($_GET['password']); ?>
+</div>
+
 </body>
 </html>
